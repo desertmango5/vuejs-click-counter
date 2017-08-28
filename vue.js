801a5c12761634b64f4904318d9f8b4c663a092c
Michael Manges <michael.manges@gmail.com>
2017-08-28 21:56:48 +0400

    Update vue.js
    Add additional messages as click count increases.

--- vue.js
+++ vue.js
@@ -10,6 +10,12 @@ var app = new Vue({
 		numClicks: function() {
 			true ? app.number++ : false
 			app.number >= 10 ? app.message2 = 'You should think about getting a life!' : undefined
+			app.number >= 25 ? app.message2 = 'There has to be a better way to spend your time!' : undefined
+			app.number >= 50 ? app.message2 = 'You\'re still here?!?' : undefined
+			app.number >= 250 ? app.message2 = 'Finger tired yet??' : undefined
+			app.number >= 500 ? app.message2 = 'At this point you may as well keep going...' : undefined
+			app.number >= 1000 ? app.message2 = '1000... Is that all you got?' : undefined
+			app.number >= 2000 ? app.message2 = 'My son\'s record is 2222!' : undefined
 		},
 		reset: function() {
 			app.number = 0
